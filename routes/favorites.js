cef5952b95c48746779baaa9ccaeba87e444c722
Jimmy Demortreux <jimmydemortreux@mbp-de-jimmy.lan>
2022-03-11 11:54:01 +0100

    route favoris

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { default: axios } = require("axios");
 const User = require("../models/User");
 
-router.get("/favorites", async (req, res) => {
+router.post("/favorites", async (req, res) => {
   try {
     const isUserConnected = await User.findOne({ token: req.fields.token });
-
+    console.log("isuserconnected", isUserConnected);
+    console.log("token", req.fields.token);
+    console.log("charct", req.fields.characters);
     if (isUserConnected === null) {
       res.status(400).json({ message: "user invalid" });
     } else {
@@ -20,9 +22,27 @@ router.get("/favorites", async (req, res) => {
         await isUserConnected.save();
       }
     }
-    console.log(isUserConnected);
-    console.log("toke n", req.fields.token);
-    console.log("charct", req.fields.characters);
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.response);
+  }
+});
+
+router.get("/favorites", async (req, res) => {
+  //   console.log(req.query.id);
+
+  try {
+    const isUserConnected = await User.findOne({ token: req.query.token });
+    console.log("isUserExist==>", isUserConnected.favCharacters);
+    if (isUserConnected === null) {
+      //   console.log("s'affiche si mon isUserExist est invalide");
+      res.status(400).json({ message: "userId invalid" });
+    } else {
+      res.status(200).json(isUserConnected.favCharacters);
+      console.log();
+    }
+    // console.log(isUserExist);
+  } catch (error) {
+    console.log(error.response);
+  }
 });
 module.exports = router;
